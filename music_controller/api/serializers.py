072645d45f801949fe088a9175acb8a1e3d0d760
nishantasarma <nishantasarma@gmail.com>
2021-02-17 21:46:06 +0530

    Added Room Serializer

---
+++ music_controller/api/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .model import Room
+class RoomSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Room
+        #id is primary key automatically created on every model in django
+        fields = ('id', 'code', 'host','guest_can_pause','votes_to_skip', 'created_at')
